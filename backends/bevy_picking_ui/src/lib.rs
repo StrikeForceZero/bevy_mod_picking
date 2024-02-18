92694dc9b05212599e49786fddf0d54ac96257d6
bstriker <strikeforcezero@gmail.com>
2024-02-18 02:06:53 -0500

    Replace WorldQuery with QueryData

--- backends/bevy_picking_ui/src/lib.rs
+++ backends/bevy_picking_ui/src/lib.rs
@@ -23,7 +23,7 @@
 #![deny(missing_docs)]
 
 use bevy_app::prelude::*;
-use bevy_ecs::{prelude::*, query::WorldQuery};
+use bevy_ecs::{prelude::*, query::QueryData};
 use bevy_math::prelude::*;
 use bevy_render::{camera::NormalizedRenderTarget, prelude::*};
 use bevy_transform::prelude::*;
@@ -47,8 +47,8 @@ impl Plugin for BevyUiBackend {
 }
 
 /// Main query from bevy's `ui_focus_system`
-#[derive(WorldQuery)]
-#[world_query(mutable)]
+#[derive(QueryData)]
+#[query_data(mutable)]
 pub struct NodeQuery {
     entity: Entity,
     node: &'static Node,
